725089df18212cc7db8978f372e1722bc2dcd07a
Pavlo <malovanniy@gmail.com>
2022-06-21 14:08:24 +0300

    fix error with using small images

--- storage/localstorage.go
+++ storage/localstorage.go
@@ -19,7 +19,11 @@ func (store *localdb) Add(value image.Image, ext string) error {
 	x := value.Bounds().Size().X
 	for res := 25; res <= 100; res += 25 {
 		// Resize Image
-		file := file{resize.Resize(uint((x*res)/100), 0, value, resize.Lanczos3), ext}
+		Size := uint((x * res) / 100)
+		if Size < 100 {
+			Size = 100
+		}
+		file := file{resize.Resize(Size, 0, value, resize.Lanczos3), ext}
 		// Add to storage resized image {id: id, quality: quality}
 		store.Storage[fileName{Name: strconv.Itoa(store.CurrentId), Quality: strconv.Itoa(res)}] = file
 	}
